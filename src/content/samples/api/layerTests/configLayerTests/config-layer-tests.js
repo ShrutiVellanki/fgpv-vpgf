b9fbe7547ebbb3abd793f4632392fb06352cf49b
Vellanki <Shruti.Vellanki@ec.gc.ca>
2018-05-11 10:49:57 -0400

    test(api): added zoom tests to configLayer tests

--- src/content/samples/api/layerTests/configLayerTests/config-layer-tests.js
+++ src/content/samples/api/layerTests/configLayerTests/config-layer-tests.js
@@ -113,8 +113,10 @@ $.getScript('../../../../rv-main.js', function () {
                     //enables ability to set attributes/get attributes for already downloaded attributes. 
                     document.getElementById("SetSingleAttribute").disabled = false;
                     document.getElementById("SetAllAttributes").disabled = false;
-                    document.getElementById("GetAllAttributes").disabled = false; 
+                    document.getElementById("GetAllAttributes").disabled = false;
                     document.getElementById("GetAttribute").disabled = false;
+                    document.getElementById("RemoveAttribute").disabled = false;
+                    document.getElementById("RemoveAllAttributes").disabled = false;
                 });
 
                 // subscribe to attributes changed
@@ -122,7 +124,7 @@ $.getScript('../../../../rv-main.js', function () {
                     console.log('Attributes changed');
 
                     $("button").click(function () {
-                        if(this.id === "SetSingleAttribute" || this.id === "SetAllAttributes"){
+                        if (this.id === "SetSingleAttribute" || this.id === "SetAllAttributes") {
                             document.getElementById(this.id).style.backgroundColor = "#00FF00";
                         }
                     });
@@ -136,25 +138,161 @@ $.getScript('../../../../rv-main.js', function () {
 
                 //CHANGE ATTRIBUTES: 
                 //TODO: why isn't the subscribed picking up on button action?
-                document.getElementById("SetSingleAttribute").onclick = function (){
+                document.getElementById("SetSingleAttribute").onclick = function () {
                     configLayer.setAttributes(1, 'Country', 'new Country');
+                    //disable after attribute is set, so user can't set again.
+                    document.getElementById("SetSingleAttribute").disabled = true;
                 }
 
-                document.getElementById("SetAllAttributes").onclick = function (){
+                document.getElementById("SetAllAttributes").onclick = function () {
                     configLayer.setAttributes(2, { Country: 'Country is new', OBJECTID: -1 });
+                    //disable after attribute is set, so user can't set again.
+                    document.getElementById("SetAllAttributes").disabled = true;
                 }
 
                 //GET ATTRIBUTES (no associated subscribe function): 
                 //TODO: how to get Country/county name to verify attributes
-                document.getElementById("GetAttribute").onclick = function (){
+                document.getElementById("GetAttribute").onclick = function () {
                     document.getElementById("output").innerHTML = configLayer.getAttributes(5);
                     console.log(configLayer.getAttributes(5));
                 }
 
-                document.getElementById("GetAllAttributes").onclick = function (){
+                document.getElementById("GetAllAttributes").onclick = function () {
                     document.getElementById("output").innerHTML = configLayer.getAttributes();
                 }
 
+                //REMOVE ATTRIBUTES: 
+
+                // subscribe to attributes removed --> WONKINESS HERE MIGHT BE BECAUSE GETATTRIBUTES IS CALLED AGAIN
+                //TODO: LAST CHECK FOR GETATTRIBUTES
+                configLayer.attributesRemoved.subscribe(l => {
+                    console.log('Attributes removed');
+
+                    $("button").click(function () {
+
+                        if (this.id == "RemoveAttribute") {
+                            // confirm attribute for OID 5 removed, but others still persist
+                            if (configLayer.getAttributes(5) === undefined && configLayer.getAttributes(10) !== undefined) {
+                                document.getElementById("RemoveAttribute").style.backgroundColor = "#00FF00";
+                            }
+                            else {
+                                console.log("Test failed because attribute was not removed successfully, or removed wrong attribute")
+                                document.getElementById("RemoveAttribute").style.backgroundColor = "#cd0000";
+                            }
+                        }
+
+                        if (this.id == "RemoveAllAttributes") {
+                            // confirm previously present attribute was removed, and attributes list is now empty
+                            if (configLayer.getAttributes(10) === undefined && configLayer.getAttributes().length() === 0) {
+                                document.getElementById("RemoveAllAttributes").style.backgroundColor = "#00FF00";
+                            }
+                            else {
+                                console.log("Test failed because attributes were not removed successfully")
+                                document.getElementById("RemoveAttribute").style.backgroundColor = "#cd0000";
+                            }
+                        }
+
+
+                    });
+
+                });
+
+                document.getElementById("RemoveAttribute").onclick = function () {
+                    document.getElementById("GetAttribute").disabled = true;
+                    document.getElementById("RemoveAttribute").disabled = true;
+                    // remove single attribute field of layer whose attributes are already downloaded
+                    configLayer.removeAttributes(5);
+                }
+
+                document.getElementById("RemoveAllAttributes").onclick = function () {
+
+                    document.getElementById("SetSingleAttribute").disabled = true;
+                    document.getElementById("SetAllAttributes").disabled = true;
+                    document.getElementById("GetAllAttributes").disabled = true;
+                    document.getElementById("GetAttribute").disabled = true;
+                    document.getElementById("RemoveAttribute").disabled = true;
+                    document.getElementById("RemoveAllAttributes").disabled = true;
+                    // remove single attribute field of layer whose attributes are already downloaded
+                    configLayer.removeAttributes();
+                }
+
+                //No associated subscribe function
+                document.getElementById("CheckZoomLevel").onclick = function () {
+                    if (mapi.zoom === 17) {
+                        document.getElementById("CheckZoomLevel").style.backgroundColor = "#00FF00";
+                    }
+                    else {
+                        console.log("Test failed (though it could be because user did not zoom enough).")
+                        document.getElementById("CheckZoomLevel").style.backgroundColor = "#cd0000";
+                    }
+                }
+
+                //No associated subscribe function
+                document.getElementById("CheckZoomLevel2").onclick = function () {
+                    if (mapi.zoom === 0) {
+                        document.getElementById("CheckZoomLevel2").style.backgroundColor = "#00FF00";
+                    }
+                    else {
+                        console.log("Test failed (though it could be because user did not zoom enough).")
+                        document.getElementById("CheckZoomLevel2").style.backgroundColor = "#cd0000";
+                    }
+                }
+
+                //No associated subscribe function
+                document.getElementById("PanToBoundary").onclick = function () {
+
+                    // turn on the bounding box for the layer whose boundary is being panned to and then pan to layer boundary
+                    configLayer.panToBoundary();
+
+                    //expect map zoom level to have changed accordingly
+                    if (mapi.zoom === 4) {
+                        document.getElementById("PanToBoundary").style.backgroundColor = "#00FF00";
+                    }
+                    else {
+                        console.log("Test failed: Zoom level did not change after panning to boundary!")
+                        document.getElementById("PanToBoundary").style.backgroundColor = "#cd0000";
+                    }
+                }
+
+                //No associated subscribe function
+                document.getElementById("ZoomToScale").onclick = function () {
+                    // zoom to layer scale
+                    configLayer.zoomToScale()
+
+                    //expect map zoom level to have changed accordingly
+                    if (mapi.zoom === 6) {
+                        document.getElementById("ZoomToScale").style.backgroundColor = "#00FF00";
+                        document.getElementById("ZoomToScale2").disabled = false;
+                    }
+                    else {
+                        console.log("Test failed: Zoom level did not change after zoomToScale!")
+                        console.log(mapi.zoom);
+                        document.getElementById("ZoomToScale").style.backgroundColor = "#cd0000";
+                    }
+                }
+
+                //No associated subscribe function
+                document.getElementById("ZoomToScale2").onclick = function () {
+
+                    //record current zoom, verify greater than 6
+                    const currentZoom = mapi.zoom;
+                    if (currentZoom > 6) {
+                        //zoom to layer scale again, verify nothing changed.
+                        configLayer.zoomToScale();
+                        if (mapi.zoom === currentZoom) {
+                            document.getElementById("ZoomToScale2").style.backgroundColor = "#00FF00";
+                        }
+                        else {
+                            document.getElementById("ZoomToScale2").style.backgroundColor = "#cd0000";
+                            console.log("Test failed: map zoom unexpectedly changed after zoomToScale!")
+                        }
+                    }
+                    else {
+                        document.getElementById("ZoomToScale2").style.backgroundColor = "#cd0000";
+                        console.log("Test failed: currentZoom<=6 (Though user may have accidently zoomed out).")
+                    }
+
+                }
 
             }
 
@@ -162,7 +300,7 @@ $.getScript('../../../../rv-main.js', function () {
     });
 
     $('#main').append(`
-        <div id="fgpmap" style="height:900px; width:75%; margin-left:10px" class="column" rv-langs='["en-CA", "fr-CA"]' rv-service-endpoint="http://section917.cloudapp.net:8000/""></div>
+        <div id="fgpmap" style="height:1300px; width:80%; margin-left:10px" class="column" rv-langs='["en-CA", "fr-CA"]' rv-service-endpoint="http://section917.cloudapp.net:8000/""></div>
     `);
 
     const mapInstance = new RZ.Map(document.getElementById('fgpmap'), '../../../config.rcs.[lang].json');
