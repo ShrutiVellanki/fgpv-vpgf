17143b5c3130e0b06f7f055ef18db449b784e734
Vellanki <Shruti.Vellanki@ec.gc.ca>
2018-05-17 15:57:53 -0400

    test(api): Added ability for panel to set contents

--- src/content/samples/api/new-panel/new-panel-tests.js
+++ src/content/samples/api/new-panel/new-panel-tests.js
@@ -1,35 +1,118 @@
 $.getScript('./panel-api.js', function () {
 
-    var panelCount = 0;
     var lightyear;
+    var text = "<p>My name is Buzzlightyear</p>";
+    //hidden make visible
+    var htmlInput = $("#coolInput");
+    //make visible
+    var htmlButton = $("#coolButton");
+    var panelElem1, panelElem2, panelElem3;
+
+    $("#ConstructPanelElems").click(function () {
+        panelElem1 = new PanelElem(1);
+        panelElem2 = new PanelElem(2);
+        panelElem3 = new PanelElem(3);
+        $("#SetStringElem").prop('disabled', false);
+    });
+
+    $("#SetStringElem").click(function () {
+        panelElem1.element = text;
+        $("#SetInputElem").prop('disabled', false);
+    });
+
+    $("#SetInputElem").click(function () {
+        panelElem2.element = htmlInput;
+        $("#SetButtonElem").prop('disabled', false);
+    });
+
+    $("#SetButtonElem").click(function () {
+        panelElem3.element = htmlButton;
+        $("#ConstructPanel").prop('disabled', false);
+    });
 
     $("#ConstructPanel").click(function () {
-        lightyear = new Panel("dummyMap", "Buzz Lightyear", "Ground Control");
-        $("#GetControl").prop('disabled', false);
+        lightyear = new Panel(1, "dummyMap", "Buzz Lightyear", "Ground Control");
+        //$("#GetControl").prop('disabled', false);
+        $("#GetContent").prop('disabled', false);
+        $("#GetID").prop('disabled', false);
+        $("#SetZIndex").prop('disabled', false);
+        $("#GetElement").prop('disabled', false);
+        $("#SetWidth").prop('disabled', false);
+        $("#SetHeight").prop('disabled', false);
+        $("#SetContent").prop('disabled', false);
+        $("#SetPosition").prop('disabled', false);
+        $("#AddPanel").prop('disabled', false);
+        $("#SetPositionOverflow").prop('disabled', false);
+        $("#SetWidthOverflow").prop('disabled', false);
+        $("#SetHeightOverflow").prop('disabled', false);
     });
 
     //TODO: actual Control object.
     $("#GetControl").click(function () {
         alert(lightyear.control);
         console.log(lightyear.control);
-        $("#GetID").prop('disabled', false);
     });
 
     $("#GetID").click(function () {
         alert(lightyear.id);
         console.log(lightyear.id);
-        $("#SetContentBefore").prop('disabled', false);
     });
 
-    $("#SetContentBefore").click(function () {
-        alert(lightyear.id);
-        console.log(lightyear.id);
-        //$("#GetContentBefore").prop('disabled', false);
+    $("#SetZIndex").click(function () {
+        lightyear.zindex = -1000;
+    });
+
+    $("#GetElement").click(function () {
+        alert(lightyear.element);
+        console.log(lightyear.element);
+    });
+
+    //width/height can be set to string/number, pixels/percents
+    //TODO: check for width/height going off parent div. --> doesn't seem to work when div is hidden.
+    $("#SetWidth").click(function () {
+        lightyear.width = 300;
+    });
+
+    $("#SetHeight").click(function () {
+        lightyear.height = "20%";
+    });
+
+    //if width and height cause overflow, or positions cause overflow we need to keep things on screen
+    //Idea: save width/height/positions as last set, and set it back to that. 
+    //new function: offscreen (as parameters, new width/height/position)
+    //when you change width/
+    $("#SetWidthOverflow").click(function () {
+        lightyear.width = 10000;
+    });
+
+    $("#SetHeightOverflow").click(function () {
+        lightyear.height = "800%";
+    });
+
+    $("#SetPosition").click(function () {
+        lightyear.position(20, 60, undefined, 90);
+    });
+
+    $("#SetPositionOverflow").click(function () {
+        lightyear.position(-100, 60, 90000, 90);
+    });
+
+    $("#AddPanel").click(function () {
+        lightyear.addPanel();
+    });
+
+    $("#GetContent").click(function () {
+        alert(lightyear.content)
+        console.log(lightyear.content);
+    });
+
+    $("#SetContent").click(function () {
+        lightyear.content = [panelElem1, panelElem2, panelElem3];
+        $("SetVisibleInput").prop('disabled', false);
+        $("SetVisibleButton").prop('disabled', false);
     });
 
 });
-
-
 
 
 
--- src/content/samples/api/new-panel/panel-api.js
+++ src/content/samples/api/new-panel/panel-api.js
@@ -1,137 +1,259 @@
 class Panel {
 
-    //takes in ID of parent div, id as defined by user, control as defined by user.
-    constructor(parentID, pID = undefined, control = undefined) {
+    /**
+    * Creates a new Panel.
+    * @constructor Panel
+    * @param {number} panelCount - the number of panels (including this one) there are on the map instance
+    * @param {string} parentID - the parentID of this panel (usually the map instance where panel sits)
+    * @param {(string|number)} [pID] - the user defined ID name for this panel 
+    * @param {Control} [control] - the user defined controls for this panel
+    */
+    constructor(panelCount, parentID, pID = undefined, control = undefined) {
 
-        //has panel been added?
-        this._panel_added = false;
+        //init attributes
+        this._map_added = false;
         this._parent_id = parentID;
         this._control = control;
-        //this._z_index = undefined;
+        this._content = [];
 
+        //add panel template to map/parent div
+        var panelSnippet = '<div hidden id ="panelContents" style="border: 5px solid gray; margin: 10px; position:relative; top:0px; left:0px"><div id ="panelControls"></div><div id = "panelBody"></div></div>';
+        $("#" + this._parent_id).append(panelSnippet);
 
-        //panel id as set (or not set, by the user)
-        if (pID === undefined) {
-            this._id = "undefined";
-        }
-        else {
-            this._id = pID;
-        }
-
-        //default widths and heights
-        this._width = "400px";
-        this._height = "auto";
-
-    }
-
-    addPanel(panelCount) {
-        this._panel_added = true;
-        const panelTemplate =
-            '<div id ="panelContents" style="border: 5px solid gray; margin: 10px; position:relative; top:0px; left:0px"><div id ="panelControls"></div><div id = "panelBody"></div></div>';
+        //panel template's div IDs can now be referred to by panelCount
         this._panel_contents = "panelContents" + panelCount.toString();
         this._panel_controls = "panelControls" + panelCount.toString();
         this._panel_body = "panelBody" + panelCount.toString();
-        $("#" + this._parent_id).append(panelTemplate);
+
+        //update panel template's div IDs, 
         document.getElementById("panelContents").id = this._panel_contents;
         document.getElementById("panelControls").id = this._panel_controls;
         document.getElementById("panelBody").id = this._panel_body;
-        document.getElementById(this._panel_contents).className = this._id;
+        document.getElementById(this._panel_contents).className = pID;
 
-        if(this._z_index !== undefined){
-            this.zindex = this._z_index;
-        }
-        
+        //set default widths and heights for panel
+        this.width = "400px";
+        this.height = "auto";
 
-        //set width and height for panel
-        this.width = this._width; //400px default width for panels
-        this.height = this._height; //400px default width for panels, no default height
     }
 
-    //Triggered by open button on panel
+    /**
+    * Adds panel to the map.
+    */
+    addPanel() {
+        $("#" + this._panel_contents).removeAttr('hidden');
+        this._map_added = true;
+        this.checkOutOfBounds(); //check if added map is out of bounds
+    }
+
+    /**
+    * Opens panel body.
+    */
     open() {
         $("#" + this._panel_contents).show();
         //document.getElementsByClassName("ClosePanel").innerHTML = "Close Panel";
     }
 
-    //Triggered by close button on panel
+
+    /**
+    * Closes panel body.
+    */
     close() {
         $("#" + this._panel_contents).hide();
         //document.getElementById("ClosePanel").innerHTML = "Open Panel";
 
     }
 
+    /**
+    * Returns controls for panel
+    */
     get control() {
         return this._control;
     }
 
+    get content() {
+        return this._content;
+    }
+
+    set content(content) {
+        this._content = content;
+        //TODO:
+        //for each item in content, convert to div that goes into panel "Content"
+        //maybe have line separating content. 
+
+        for (let elem of content) {
+            $("#" + this._panel_body).append(elem._element);
+        }
+    }
+
+    //id is set to class name because easier to set id property to manipulate other divs
     get id() {
-        return this._id;
+        return document.getElementById(this._panel_contents).className;
     }
 
     // Can be pixels or percent
     // Will never exceed map edge
     set width(width) {
 
-        if (this._panel_added == false) {
-            this._width = width;
+        if (typeof width === "number") {
+            document.getElementById(this._panel_contents).style.width = width.toString() + "px";
+        }
+        else if (width.slice(-2) !== "px" && width.slice(-1) !== "%") {
+            document.getElementById(this._panel_contents).style.width = width + "px";
         }
         else {
             document.getElementById(this._panel_contents).style.width = width;
-            if (document.getElementById(this._parent_id).offsetWidth < document.getElementById(this._parent_id).scrollWidth) {
-                alert("Abort mission, panel has no room! Removing Panel.")
-                document.getElementById(this._panel_contents).remove();
-            }
+        }
+
+        if (this._map_added) {
+            this.checkOutOfBounds();
         }
     }
 
+    //2 cases: panel is added to screen, can actually modify zIndex property of panel
+    //if zindex is defined BEFORE its added, need to make sure its set properly upon addition --> so in zindex added method.
+    //TODO: remove alert (only for testing purposes)
     set zindex(zindex) {
-        console.log("Z Index set!");
-        if (this._panel_added == false){
-            this._z_index = zindex;
-        }
-        else{
-            document.getElementById(this._panel_contents).style.zIndex = zindex;
-        }        
+        document.getElementById(this._panel_contents).style.zIndex = zindex;
+        alert("Z Index set to: " + zindex.toString());
     }
 
     // Can be pixels or percent
     // Will never exceed map edge
     set height(height) {
 
-        if (this._panel_added == false) {
-            this._height = height;
+        if (typeof height === "number") {
+            document.getElementById(this._panel_contents).style.height = height.toString() + "px";
+        }
+        else if (height.slice(-2) !== "px" && height.slice(-1) !== "%") {
+            document.getElementById(this._panel_contents).style.height = height + "px";
         }
         else {
             document.getElementById(this._panel_contents).style.height = height;
-            if (document.getElementById(this._parent_id).offsetHeight < document.getElementById(this._parent_id).scrollHeight) {
-                alert("Abort mission, panel has no room! Removing panel.")
-                document.getElementById(this._panel_contents).remove();
-            }
+        }
+
+        if (this._map_added) {
+            this.checkOutOfBounds();
         }
     }
 
     //returns panel shell element
+    //introduces problem here where no real way to get panel shell unless HTML snippet is in a different file.
     get element() {
         return document.getElementById(this._panel_contents);
+    }
+
+    //position defined in numbers, need to be changed toString pixels
+    position(top, left, bottom, right) {
+        if (top != undefined) {
+            document.getElementById(this._panel_contents).style.top = top.toString() + "px";
+        }
+
+        if (left != undefined) {
+            document.getElementById(this._panel_contents).style.left = left.toString() + "px";
+        }
+
+        if (right != undefined) {
+            document.getElementById(this._panel_contents).style.right = right.toString() + "px";
+        }
+
+        if (bottom != undefined) {
+            document.getElementById(this._panel_contents).style.bottom = bottom.toString() + "px";
+        }
+
+        if (this._map_added) {
+            this.checkOutOfBounds();
+        }
+
+    }
+
+    //Auto sets width and height to 20% of parent div
+    //If position was out of bounds, autosets to default position (remember position ALWAYS IN PIXELS)
+    checkOutOfBounds() {
+
+        if (document.getElementById(this._parent_id).clientHeight < document.getElementById(this._parent_id).scrollHeight) {
+            alert("Panel height too big: changing to 20%")
+            document.getElementById(this._panel_contents).style.height = "20%"
+        }
+
+        if (document.getElementById(this._parent_id).clientWidth < document.getElementById(this._parent_id).scrollWidth) {
+            alert("Panel width too big: changing to 20%")
+            document.getElementById(this._panel_contents).style.width = "20%"
+        }
+
+        var parent_position = $("#" + this._parent_id).position();
+        var pt = parent_position.top; //"parent top"
+        var pl = parent_position.left;
+        var pr = parent_position.right;
+        var pb = parent_position.bottom;
+
+        var panel_position = $("#" + this._panel_contents).position();
+        var ct = panel_position.top; //"child top"
+        var cl = panel_position.left;
+        var cr = panel_position.right;
+        var cb = panel_position.bottom;
+
+
+        //"if the top, left of child less then parent OR bottom, right of child greater than parent"
+        if (ct < pt || cl < pl || cr > pr || cb > pb) {
+            alert("Panel position out of bounds: changing to default")
+            document.getElementById(this._panel_contents).style.top = "0px";
+            document.getElementById(this._panel_contents).style.left = "0px";
+        }
     }
 
 }
 
 class PanelElem {
 
+    //pregenerated id --> "pe and elemNum"
+    constructor(elemNum) {
+        //this._visible = true;
+        this._id = "pe" + elemNum.toString();
+    }
+
     //element can be string, HTMLElement, jQuery<HTMLElement>
     //TODO: jQuery<HTMLElement>
     set element(element) {
+
+
         if (typeof element === "string") {
             this._element = $.parseHTML(element);
+            //console.log(this._element);
         }
-        else if (typeof element === "HTMLElement") {
+        else {
             this._element = element;
+            if (element.id !== undefined) {
+                this._id = element.id;
+            }
+        }
+
+        //this._element_set = true;
+
+
+
+        //changes actual element visibility once element set
+        //this.visible = this._visible;
+    }
+
+    //sets visibility even if element itself is not set --> if set changes actual visibility
+    /*set visible(visible) {
+        this._visible = visible;
+
+        if(this._element_set && this._visible == false){
+            this._element.style.visibility = "hidden";
+        }
+        else if (this._element_set && this._visible == true){
+            this._element.style.visibility = "visible";
         }
     }
 
-    set visible(visible) {
-        this._visible = visible;
+    get visible(){
+        return this._visible;
+    }*/
+
+    get id() {
+        return this._id;
     }
 
 }
@@ -139,7 +261,7 @@ class PanelElem {
 //Control buttons come with open/close for whole panel
 //set visible for each PanelElem OOHHHHHHHHHHH. 
 class Btn extends PanelElem {
-    set element() {
+    set element(element) {
         throw "Exception: Btn Elements cannot be set!"
     }
 }
